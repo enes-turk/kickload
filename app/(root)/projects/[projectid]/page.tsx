b1ba6d6341f9d143e45f0634b9d1cee13f304c72
Enes Türk <partuminfernum@gmail.com>
2025-01-26 23:11:36 +0100

    small changes

--- app/(root)/projects/[projectid]/page.tsx
+++ app/(root)/projects/[projectid]/page.tsx
@@ -7,16 +7,16 @@ import data from "@/public/data_1.json"; // Adjust the path as necessary
 const Page = () => {
   const pathname = usePathname();
   const projectid = pathname.split("/").pop();
-
   // Parse the project id from the JSON data
   const projectData = data.find((item) => item.id === projectid);
 
   return (
-    <div>
+    <div className="project-page text-4xl font-bold ">
       <h1>
         You are in a page of project:{" "}
         {projectData ? projectData.name : "Not Found"}{" "}
       </h1>
+      <h1>{projectData ? projectData["date-posted"] : "Not Found"} </h1>
     </div>
   );
 };
